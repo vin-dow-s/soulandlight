02f5e653162d89d47dc04a0bde344583346bd954
Vincent Seube <107034608+vin-dow-s@users.noreply.github.com>
2025-03-08 16:37:16 +0100

    No unescaped rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,22 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from "@eslint/eslintrc"
+import { dirname } from "path"
+import { fileURLToPath } from "url"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+    baseDirectory: __dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
 
-export default eslintConfig;
+    {
+        rules: {
+            "react/no-unescaped-entities": "off",
+        },
+    },
+]
+
+export default eslintConfig
