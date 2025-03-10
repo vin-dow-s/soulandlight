e8057dc699398cfdc9f68561be197d64209a52ed
Vincent Seube <107034608+vin-dow-s@users.noreply.github.com>
2025-03-10 21:32:14 +0100

    About fix

--- src/app/(app)/components/sections/About.tsx
+++ src/app/(app)/components/sections/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <section
             id="a-propos-section"
-            className="body-text flex flex-col lg:w-[75%] sm:w-[90%] w-[95%] mx-auto justify-center items-center sm:my-64 my-16 px-2"
+            className="body-text flex flex-col lg:w-[75%] sm:w-[90%] w-[95%] mx-auto justify-center items-center sm:my-64 my-16 px-4"
         >
             <h2
                 id="a-propos"
@@ -14,11 +14,11 @@ const About = () => {
                 À propos
             </h2>
             <div className="flex flex-col xl:flex-row gap-8 ">
-                <div>
+                <div className="w-full">
                     <Image
                         src="/assets/pascal2.webp"
                         alt="Photo Pascal"
-                        className="rounded-lg max-w-[650px] min-w-[400px] w-full"
+                        className="rounded-lg max-w-[600px] xl:min-w-[400px] w-full"
                         width={650}
                         height={650}
                     />
