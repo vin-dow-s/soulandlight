f1cf555b5ee256af2c7b0466fb616e1176425da3
Vincent Seube <107034608+vin-dow-s@users.noreply.github.com>
2025-03-10 21:39:23 +0100

    H4 to H5 fix

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -12,9 +12,9 @@ const HomePage = () => {
             <Testimonials />
             <About />
             <div className="flex flex-col justify-center items-center gap-6 mt-96 mb-64">
-                <h4 className="text-sm sm:text-base font-main">
+                <h5 className="text-sm sm:text-base font-main">
                     Vous avez des questions ou envie d'en savoir plus ?
-                </h4>
+                </h5>
                 <ButtonLink href="/contact" variant="secondary">
                     Contactez-moi
                 </ButtonLink>
